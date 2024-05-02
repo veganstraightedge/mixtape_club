05b5b62c58c156dc38893baaa2948a030f94f93e
Shane Becker <veganstraightedge@gmail.com>
2024-05-01 17:39:05 -0700

    Beginning dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,8 @@
 class DashboardController < ApplicationController
-  def index; end
+  def index
+    @draft_mixtapes     = Current.user.mixtapes.draft
+    @secret_mixtapes    = Current.user.mixtapes.secret
+    @published_mixtapes = Current.user.mixtapes.published
+    @archived_mixtapes  = Current.user.mixtapes.archived
+  end
 end
